61dd9fedfb9edba50d185c13e9ff6f7010ad6307
Calin Georgescu <C.A.Georgescu@tudelft.nl>
2024-09-01 22:56:44 +0200

    Update static html path in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,4 +37,4 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ["source/_static"]
+html_static_path = ["_static"]
