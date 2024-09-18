d4ec969bfe07e26ed5845f25505b236c049b4100
Calin Georgescu <caling@protonmail.com>
2024-09-18 13:25:48 +0200

    Make space-time collision operator visible in components module

--- qlbm/components/spacetime/__init__.py
+++ qlbm/components/spacetime/__init__.py
@@ -1,9 +1,11 @@
+from .collision import SpaceTimeCollisionOperator
 from .initial_conditions import SpaceTimeInitialConditions
 from .measurement import SpaceTimeGridVelocityMeasurement
 from .spacetime import SpaceTimeQLBM
 from .streaming import SpaceTimeStreamingOperator
 
 __all__ = [
+    "SpaceTimeCollisionOperator",
     "SpaceTimeInitialConditions",
     "SpaceTimeStreamingOperator",
     "SpaceTimeQLBM",
