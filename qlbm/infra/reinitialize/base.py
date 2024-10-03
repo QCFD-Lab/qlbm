3aaff4212ef0fa642caf847a9cc020372912af22
Calin Georgescu <C.A.Georgescu@tudelft.nl>
2024-10-03 09:35:08 +0200

    Add reinitialization documentation

--- qlbm/infra/reinitialize/base.py
+++ qlbm/infra/reinitialize/base.py
@@ -12,6 +12,30 @@ from qlbm.lattice import Lattice
 
 
 class Reinitializer(ABC):
+    """
+    Base class for all algorithm-specific reinitializers.
+    A ``Reinitializer`` uses the information at information available
+    at the end of the simulation of 1 or more time steps
+    to new initial conditions for the following time steps.
+    Such information includes the quantum state and counts extracted from it.
+    Novel initial conditions are inferred automatically based on
+    the requirements of the algorithm under simulation, and
+    an on-the-fly :class:`.CircuitCompiler` automatically converts
+    them to the appropriate format to enable compatibility with the already transpiled circuits.
+    For convenience, all reinitializers provide a uniform :meth:`reinitialize` interface,
+    which takes as input both the quantum state and the counts performed during simulation.
+    Its implementation may choose to ignore one of those inputs, depending on the
+    algorithm and implementation.
+
+    =========================== ======================================================================
+    Attribute                   Summary
+    =========================== ======================================================================
+    :attr:`lattice`             The :class:`.Lattice` of the simulated system.
+    :attr:`compiler`            The compiler that converts the novel initial conditions circuits.
+    :attr:`logger`              The performance logger, by default ``getLogger("qlbm")``
+    =========================== ======================================================================
+    """
+
     lattice: Lattice
 
     def __init__(
@@ -32,8 +56,36 @@ class Reinitializer(ABC):
         backend: AerBackend | None,
         optimization_level: int = 0,
     ) -> QiskitQC | QulacsQC:
+        """
+        Parses the input statevector and counts, constructs a new initial conditions circuit, and transpiles it to the given backend.
+
+        Parameters
+        ----------
+        statevector : Statevector
+            The statevector at the end of the simulation.
+        counts : Counts
+            The counts extracted from the statevector at the end of the simulation.
+        backend : AerBackend | None
+            The backend to compile to.k
+        optimization_level : int, optional
+            The optimization level to pass to the circuit compiler, by default 0.
+
+        Returns
+        -------
+        QiskitQC | QulacsQC
+            The compiled initial conditions circuit to use for the next time step.
+        """
         pass
 
     @abstractmethod
     def requires_statevector(self) -> bool:
+        """
+        Whether the reinitializer requires a copy of the statevector.
+        Omotting the statevector may significantly increase the perfomance of reinitialization.
+
+        Returns
+        -------
+        bool
+            Whether a statevector is needed.
+        """
         pass
--- qlbm/infra/reinitialize/collisionless_reinitializer.py
+++ qlbm/infra/reinitialize/collisionless_reinitializer.py
@@ -14,6 +14,25 @@ from .base import Reinitializer
 
 
 class CollisionlessReinitializer(Reinitializer):
+    """
+    :class:`.CQLBM`-specific implementation of the :class:`.Reinitializer`.
+    Compatible with both :class:`.QiskitRunner`\ s and :class:`.QulacsRunner`\ s.
+    To generate a new set of initial conditions for the CQLBM algorithm,
+    the reinitializer simply returns the quantum state computed
+    at the end of the previous simulation.
+    This allows the reuse of a single quantum circuit for the simulation
+    of arbitrarily many time steps.
+    No copy of the statevector is required.
+
+    =========================== ======================================================================
+    Attribute                   Summary
+    =========================== ======================================================================
+    :attr:`lattice`             The :class:`.CollisionlessLattice` of the simulated system.
+    :attr:`compiler`            The compiler that converts the novel initial conditions circuits.
+    :attr:`logger`              The performance logger, by default ``getLogger("qlbm")``
+    =========================== ======================================================================
+    """
+
     lattice: CollisionlessLattice
     statevector: Statevector
     counts: Counts
@@ -35,6 +54,26 @@ class CollisionlessReinitializer(Reinitializer):
         backend: AerBackend | None = None,
         optimization_level: int = 0,
     ) -> QiskitQC | QulacsQC:
+        """
+        Returns the provided ``statevector`` as a new Qiskit ``Initialize`` object
+        that can be prepended to the time step circuit to resume simulation.
+
+        Parameters
+        ----------
+        statevector : Statevector
+            The statevector at the end of the simulation.
+        counts : Counts
+            Ignored.
+        backend : AerBackend | None
+            Ignored.
+        optimization_level : int, optional
+            Ignored.
+
+        Returns
+        -------
+        QiskitQC | QulacsQC
+            A Qiskit ``Initialize`` object.
+        """
         circuit = self.lattice.circuit.copy()
         circuit.compose(
             Initialize(statevector),
