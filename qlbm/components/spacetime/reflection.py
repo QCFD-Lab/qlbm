bb717a4b8cb480695f9f08e7c804d50999bda1d5
Calin Georgescu <caling@protonmail.com>
2024-12-10 12:07:42 +0100

    Add D2Q4 pointwise STQBM reflection

--- qlbm/components/spacetime/reflection.py
+++ qlbm/components/spacetime/reflection.py
@@ -84,11 +84,47 @@ class SpaceTimeReflectionOperator(SpaceTimeOperator):
 
                 if grid_qubit_indices_to_invert:
                     circuit.x(grid_qubit_indices_to_invert)
-                circuit.barrier()
         return circuit
 
     def __create_circuit_d2q4(self) -> QuantumCircuit:
-        raise CircuitException("Reflection Operator unsupported for D2Q4.")
+        circuit = self.lattice.circuit.copy()
+
+        for block in self.blocks:
+            for reflection_data in block.get_spacetime_reflection_data_d2q4(
+                self.lattice.properties, self.timestep
+            ):
+                grid_qubit_indices_to_invert = [
+                    self.lattice.grid_index(0)[0] + qubit
+                    for qubit in reflection_data.qubits_to_invert
+                ]
+
+                if grid_qubit_indices_to_invert:
+                    # Inverting the qubits that are 0 turns the
+                    # Dimensional grid qubit state encoding this wall to |11...1>
+                    # Which in turn allows us to control on this row, in combination with the comparator
+                    circuit.x(grid_qubit_indices_to_invert)
+
+                control_qubits = self.lattice.grid_index()
+                target_qubits = [
+                    self.lattice.velocity_index(
+                        neighbor_velocity_pair[0],
+                        neighbor_velocity_pair[1],
+                    )[0]
+                    for neighbor_velocity_pair in reflection_data.neighbor_velocity_pairs
+                ]
+
+                # Controlled swap decompositions
+                circuit.cx(target_qubits[1], target_qubits[0])
+                circuit.compose(
+                    MCMT(XGate(), len(control_qubits) + 1, len(target_qubits) - 1),
+                    qubits=control_qubits + target_qubits,
+                    inplace=True,
+                )
+                circuit.cx(target_qubits[1], target_qubits[0])
+
+                if grid_qubit_indices_to_invert:
+                    circuit.x(grid_qubit_indices_to_invert)
+        return circuit
 
     def __str__(self) -> str:
         # TODO: Implement
