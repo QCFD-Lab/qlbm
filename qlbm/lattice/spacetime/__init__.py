2afa8b1086087e9d2469951882d1e62fcbada627
Calin Georgescu <caling@protonmail.com>
2025-01-15 18:25:58 +0100

    Add STQBM lattice properties module documentation

---
+++ qlbm/lattice/spacetime/__init__.py
@@ -0,0 +1,6 @@
+""":math:`D_2Q_4` STQBM builder."""
+
+from .d1q2 import D1Q2SpaceTimeLatticeBuilder
+from .d2q4 import D2Q4SpaceTimeLatticeBuilder
+
+__all__ = ["D1Q2SpaceTimeLatticeBuilder", "D2Q4SpaceTimeLatticeBuilder"]
