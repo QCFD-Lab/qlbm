eda0947b437ec0cbaaec548011ef85c13267dca4
Calin Georgescu <caling@protonmail.com>
2025-07-14 14:34:48 +0200

    Update result documentation

--- qlbm/infra/result/lqlga_result.py
+++ qlbm/infra/result/lqlga_result.py
@@ -1,4 +1,4 @@
-""":class:`.SpaceTimeQLBM`-specific implementation of the :class:`.QBMResult`."""
+""":class:`.LQLGA`-specific implementation of the :class:`.QBMResult`."""
 
 import re
 from os import listdir
@@ -15,10 +15,31 @@ from .base import QBMResult
 
 
 class LQLGAResult(QBMResult):
+    """
+    :class:`.LQLGA`-specific implementation of the :class:`.QBMResult`.
+
+    Processes counts sampled from :class:`.LQLGAGridVelocityMeasurement` primitives.
+
+    =========================== ======================================================================
+    Attribute                   Summary
+    =========================== ======================================================================
+    :attr:`lattice`             The :class:`.LQLGA` of the simulated system.
+    :attr:`directory`           The directory to which the results outputs data to.
+    :attr:`output_file_name`    The root name for files containing time step artifacts, by default "step".
+    =========================== ======================================================================
+    """
+
     num_steps: int
+    """The time step to which this result corresponds."""
+
     directory: str
+    """The output directory for the results."""
+
     output_file_name: str
+    """The name of the file to output the artifacts to."""
+
     lattice: LQLGALattice
+    """The lattice the result corresponds to."""
 
     def __init__(
         self,
@@ -36,16 +57,6 @@ class LQLGAResult(QBMResult):
         create_vis: bool = True,
         save_array: bool = False,
     ):
-        dimension_bit_counts = (
-            self.lattice.num_gridpoints[0].bit_length(),
-            self.lattice.num_gridpoints[1].bit_length()
-            if self.lattice.num_dims > 1
-            else 0,
-            self.lattice.num_gridpoints[2].bit_length()
-            if self.lattice.num_dims > 2
-            else 0,
-        )
-
         if self.lattice.num_dims == 1:
             # The second dimension is a dirty rendering trick for VTK and Paraview
             count_history = np.zeros((self.lattice.num_gridpoints[0] + 1, 2))
