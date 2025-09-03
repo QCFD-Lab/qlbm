ce10060c02c6f0192eb624bbe3687de3f598dd3e
Calin Georgescu <c.a.georgescu@tudelft.nl>
2025-09-03 22:28:09 +0200

    Fix typo

--- qlbm/components/lqlga/__init__.py
+++ qlbm/components/lqlga/__init__.py
@@ -13,6 +13,6 @@ __all__ = [
     "LQLGA",
     "LQLGAGridVelocityMeasurement",
     "LQLGAReflectionOperator",
-    "LQLGAMGReflectionOperator"
+    "LQLGAMGReflectionOperator",
     "LQLGAStreamingOperator",
 ]
