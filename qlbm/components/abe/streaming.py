31618aa68d567c3a870f5f4dcaa50f733de13532
Calin Georgescu <c.a.georgescu@tudelft.nl>
2025-11-04 15:22:19 +0100

    Add d2q9 streaming for AB lattice

--- qlbm/components/abe/streaming.py
+++ qlbm/components/abe/streaming.py
@@ -16,6 +16,7 @@ from qlbm.lattice.lattices.abe_lattice import ABELattice
 from qlbm.lattice.spacetime.properties_base import LatticeDiscretization
 from qlbm.tools import CircuitException, bit_value
 from qlbm.tools.exceptions import LatticeException
+from qlbm.tools.utils import get_qubits_to_invert
 
 
 class ABEStreamingOperator(LBMOperator):
@@ -26,9 +27,12 @@ class ABEStreamingOperator(LBMOperator):
     def __init__(
         self,
         lattice: ABELattice,
+        additional_control_qubit_indices: List[int] = [],
         logger: Logger = getLogger("qlbm"),
     ) -> None:
         super().__init__(lattice, logger)
+
+        self.additional_control_qubit_indices = additional_control_qubit_indices
 
         self.logger.info(f"Creating circuit {str(self)}...")
         circuit_creation_start_time = perf_counter_ns()
@@ -42,9 +46,14 @@ class ABEStreamingOperator(LBMOperator):
         if self.lattice.discretization == LatticeDiscretization.D1Q3:
             return self.__create_circuit_d1q3()
 
-        raise LatticeException("ABE only currently supported in D1Q3")
+        if self.lattice.discretization == LatticeDiscretization.D2Q9:
+            return self.__create_circuit_d2q9()
+
+        raise LatticeException("ABE only currently supported in D1Q3 and D2Q9")
 
     def __create_circuit_d1q3(self):
+        # TODO Remove?
+        # TODO add geometry
         circuit = self.lattice.circuit.copy()
 
         circuit.compose(
@@ -95,7 +104,69 @@ class ABEStreamingOperator(LBMOperator):
         )
 
         return circuit
-    
+
+    def __create_circuit_d2q9(self):
+        circuit = self.lattice.circuit.copy()
+
+        dim_indices = [
+            [
+                [1, 5, 8],  # f1, f5, f8 x <- x + 1
+                [3, 6, 7],  # f3, f6, f7 x <- x - 1
+            ],
+            [
+                [2, 5, 6],  # f2, f5, f6 y <- y + 1
+                [4, 7, 8],  # f4, f7, f8 y <- y + 1
+            ],
+        ]
+
+        for dim, dim_population_to_update in enumerate(dim_indices):
+            circuit.compose(
+                QFT(len(self.lattice.grid_index(dim))),
+                qubits=self.lattice.grid_index(dim),
+                inplace=True,
+            )
+
+            for direction, indices in enumerate(dim_population_to_update):
+                positive = bool(1 - direction)
+
+                for index in indices:
+                    velocity_inversion_qubits = [
+                        self.lattice.num_grid_qubits + q
+                        for q in get_qubits_to_invert(
+                            index, self.lattice.num_velocity_qubits
+                        )
+                    ]
+                    if velocity_inversion_qubits:
+                        circuit.x(velocity_inversion_qubits)
+
+                    circuit.compose(
+                        PhaseShift(
+                            num_qubits=len(self.lattice.grid_index(dim)),
+                            positive=positive,
+                            logger=self.logger,
+                        )
+                        .circuit.control(
+                            self.lattice.num_velocity_qubits
+                            + len(self.additional_control_qubit_indices)
+                        )
+                        .decompose(),
+                        qubits=self.additional_control_qubit_indices
+                        + self.lattice.velocity_index()
+                        + self.lattice.grid_index(dim),
+                        inplace=True,
+                    )
+
+                    if velocity_inversion_qubits:
+                        circuit.x(velocity_inversion_qubits)
+
+            circuit.compose(
+                QFT(len(self.lattice.grid_index(dim)), inverse=True),
+                qubits=self.lattice.grid_index(dim),
+                inplace=True,
+            )
+
+        return circuit
+
     @override
     def __str__(self) -> str:
         return f"[Operator ABEStreaming with lattice {self.lattice}]"
